b21c000a7ba018c5e72c8776f9d7043b73b71174
Hojiakbardevs <abdulhakimovhojiakbar123@gmail.com>
2024-06-13 16:02:22 +0500

    hamma darslar

---
+++ week7/app.js
@@ -0,0 +1,6 @@
+"use strict";
+const username = 'Hojiakbar';
+console.log(username);
+const numbers = 3.124;
+console.log(numbers);
+console.log("Salom dunyo");
---
+++ week7/app.ts
@@ -0,0 +1,5 @@
+const username: string = 'Hojiakbar';
+console.log(username);
+const numbers:number = 3.124;
+console.log(numbers);
+console.log("Salom dunyo");
