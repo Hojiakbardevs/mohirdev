bb5d7337bd8af1bb2b418a3c4ee7c1bfc9a79630
Hojiakbardevs <abdulhakimovhojiakbar123@gmail.com>
2024-06-14 09:34:23 +0500

    modules

---
+++ week7/identity.ts
@@ -0,0 +1,42 @@
+// const username: string = 'Hojiakbar';
+// console.log(username);
+// const numbers:number = 3.124;
+// console.log(numbers);
+// console.log("Salom dunyo");
+// const arar : number[] = [1,2,34,3]
+// function printGreeting(firstname: string): void {
+//     console.log(`Hello, ${firstname}!`);
+//     }
+//     printGreeting('Ulugbek');
+// console.log(arar.fill(23));
+// function identity(arg: number): number;
+// function identity(arg: string | number): string | number;
+// function identity(arg: unknown): unknown {
+//     return arg;
+// }
+// // function identity(arg: any): any;
+// function identity<Type>(arg: Type): Type {
+//     return arg;
+// }
+// // Ommabop ko'rinishi
+// function identity<T>(arg: T): T {
+//     return arg;
+// }
+
+// namespace MathOperations {
+//     export function add(a: number, b: number): number {
+//     return a + b;
+//     }
+//     }
+//     const result = MathOperations.add(2, 3);
+
+
+// // math.ts
+// export function add(a: number, b: number): number {
+//     return a + b;
+//     }
+
+//     // main.ts
+// import { add } from './math.js';
+// const result = add(5, 3);
+
