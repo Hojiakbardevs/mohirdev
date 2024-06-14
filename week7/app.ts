ff68207e70bbd83c3461bc05e891a69a90cf8b14
Hojiakbardevs <abdulhakimovhojiakbar123@gmail.com>
2024-06-14 09:18:19 +0500

    typescriptni o'rnatishni o'rganish

--- week7/app.ts
+++ week7/app.ts
@@ -1,5 +1,13 @@
-const username: string = 'Hojiakbar';
-console.log(username);
-const numbers:number = 3.124;
-console.log(numbers);
-console.log("Salom dunyo");
+// const username: string = 'Hojiakbar';
+// console.log(username);
+// const numbers:number = 3.124;
+// console.log(numbers);
+// console.log("Salom dunyo");
+const arar : number[] = [1,2,34,3]
+function printGreeting(firstname: string): void {
+    console.log(`Hello, ${firstname}!`);
+    }
+    printGreeting('Ulugbek');
+
+
+console.log(arar.fill(23));
