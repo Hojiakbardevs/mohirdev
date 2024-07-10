ed64630c884828ec3158c577c008db1af76e7595
Hojiakbardevs <abdulhakimovhojiakbar123@gmail.com>
2024-07-10 21:19:41 +0500

    learning

---
+++ week13/src/App.tsx
@@ -0,0 +1,19 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="container mx-auto">
+        <div className="columns-2">
+          <p>Well, let me tell you something, ...</p>
+          <p className="break-after-column">Sure, go ahead, laugh...</p>
+          <p>Maybe we can live without...</p>
+          <p>Look. If you think this is...</p>
+          
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
